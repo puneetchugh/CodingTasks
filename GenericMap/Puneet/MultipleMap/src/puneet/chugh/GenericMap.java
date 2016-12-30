1886dc1e4e4debcf180809b54888211248c8c6e3
Puneet Chugh <puneet.chugh@nyu.edu>
2016-12-30 01:10:27 -0500

    Interface created with One concrete class as Grocery Store

--- GenericMap/Puneet/MultipleMap/src/puneet/chugh/GenericMap.java
+++ GenericMap/Puneet/MultipleMap/src/puneet/chugh/GenericMap.java
@@ -1,5 +1,11 @@
 package puneet.chugh;
 
-public class GenericMap {
+import java.util.Collection;
+import java.util.Map;
 
+public interface GenericMap<K, V> extends Map<K,Collection<V>>{
+
+	boolean insertItem(K key, V value);
+	
+	Collection<V> getItems(K key);
 }
